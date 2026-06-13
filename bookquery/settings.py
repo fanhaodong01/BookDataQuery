39a8a1bd9c988d3c9f150e8866f7adcb52e9029d
LenLegWin <1419611674@qq.com>
2026-06-13 23:27:13 +0800

    V1.1 修改数据库为mysql

--- bookquery/settings.py
+++ bookquery/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'bookquery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bookquery',
+        'USER': 'root',
+        'PASSWORD': 'fhd000101',
+        'HOST': '192.168.1.78',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
