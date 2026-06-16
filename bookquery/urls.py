1cf7124a2542fe353b6b56143248771124b1fcf4
LenLegWin <1419611674@qq.com>
2026-06-16 23:05:02 +0800

    adapt drf: 前端改造

--- bookquery/urls.py
+++ bookquery/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from books import views
 
@@ -7,4 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/books/', views.BookSearchAPIView.as_view(), name='book_search'),
     path('api/books/<str:isbn>/', views.BookDetailAPIView.as_view(), name='book_detail'),
+    path('detail/<str:isbn>/', TemplateView.as_view(template_name='books/detail.html'), name='book_detail_page'),
+    path('', TemplateView.as_view(template_name='books/index.html'), name='index'),
 ]
